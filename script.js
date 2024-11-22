9a5b150389c50fd86c7855ebdfcdee0d7c856397
Codersweta7 <sweta803pandey@gmail.com>
2024-11-22 16:36:15 +0530

    SecureStep

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".multiword",{
+    strings:["Developing for enthusiastic learners"],
+        typespeed:300,
+        backspeed:200,
+        backDelay:2000,
+        loop:true
+    
+    
+
+})
