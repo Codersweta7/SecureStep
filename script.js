7da26007921e02d0412f082895a11f82f345319a
aatreyee <aatreyeedev05@gmail.com>
2024-11-24 17:34:58 +0530

    mobile responsive

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
-var typed = new Typed(".multiword",{
-    strings:["Developing for enthusiastic learners"],
-        typespeed:50,
-        backspeed:10,
-        backDelay:2000,
-        loop:true
-})
-const hamburgerBtn = document.getElementById('hamburger-btn');
-const navbar=document.querySelector('.navbar');
-
-hamburgerBtn.addEventListener('click',()=>{
-    navbar.classList.toggle('active');
-
+var typed = new Typed(".multiword", {
+    strings: ["Developing for enthusiastic learners"],
+    typeSpeed: 50,
+    backSpeed: 10,
+    backDelay: 2000,
+    loop: true
 });
+
+const hamburgerBtn = document.getElementById('hamburger-btn');
+const navbar = document.querySelector('.navbar');
+
+hamburgerBtn.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
+
 const themeToggleBtn = document.getElementById('theme-toggle');
-const body= document.body;
+const body = document.body;
 
 const savedTheme = localStorage.getItem('theme');
-if(savedTheme){
+if (savedTheme) {
     body.classList.add(savedTheme);
-    themeToggleBtn.textContent = savedTheme === 'darkTheme' ? 'Dark Mode' :'Light Mode';
-}
-else{
+    themeToggleBtn.textContent = savedTheme === 'dark-theme' ? 'Light Mode' : 'Dark Mode';
+} else {
     body.classList.add('dark-theme');
-    themeToggleBtn.textContent = 'Light Mode'
+    themeToggleBtn.textContent = 'Light Mode';
 }
 
 themeToggleBtn.addEventListener('click', () => {
     body.classList.toggle('dark-theme');
     const isDarkTheme = body.classList.contains('dark-theme');
     themeToggleBtn.textContent = isDarkTheme ? 'Light Mode' : 'Dark Mode';
-
     localStorage.setItem('theme', isDarkTheme ? 'dark-theme' : 'light-theme');
 });
 
 const backToTopBtn = document.getElementById("backToTopBtn");
 
-window.onscroll = function() {
+window.onscroll = function () {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         backToTopBtn.style.display = "block";
     } else {
